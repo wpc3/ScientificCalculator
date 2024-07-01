04592d7b9d615fb32b056d5a7775449a11f74ac5
Devi <diptikadevi@gmail.com>
2024-06-30 22:45:34 -0400

    help is workinggit add .' '

--- src/main/java/com/zipcodewilmington/scientificcalculator/HelpPage.java
+++ src/main/java/com/zipcodewilmington/scientificcalculator/HelpPage.java
@@ -7,15 +7,15 @@ public class HelpPage {
         return helpOperators;
     }
 
-    public static String helpOperator(String helpOperators){
-       switch (helpOperators) {
+    public String helpOperator(String helpOperators){
+        switch (helpOperators) {
            case "help":
-           case "help me":
-           case "h":
-               String help =
+//           case "help me":
+//           case "h":
+              // String help =
+               return
                        "<*><*><*><*><*><*><*><*><*><*><*><*>" + "\n" +
                        "You can specificy if you need: " + "\n" +
-                       "\"--DEG\" to switch to degrees " + "\n" +
                        "\"--operator help\" for operator" + "\n" +
                        "symbols" + "\n" +
                        "\"--memory help\" for storing or  " + "\n" +
@@ -23,9 +23,10 @@ public class HelpPage {
                        "\"--trig help\" for trig operators" + "\n" +
                        "\"--log help\" for log operators" + "\n" +
                        "<*><*><*><*><*><*><*><*><*><*><*><*>";
-               return help;
+            //   return help;
            case "--operator help":
-               String oppHelp =
+             //  String oppHelp =
+               return
                        "<*><*><*><*><*><*><*><*><*><*><*><*>" + "\n" +
                        "operator symbols are as follows: " + "\n" +
                        "\"+ or - or / or *\"  for basic  " + "\n" +
@@ -38,37 +39,38 @@ public class HelpPage {
                        "\"^\" for power " + "\n" +
                        "\"exp\" for exponential " + "\n" +
                        "<*><*><*><*><*><*><*><*><*><*><*><*>";
-               return oppHelp;
+              // return oppHelp;
            case "--memory help":
-               String memHelp =
+             //  String memHelp =
+               return
                        "<*><*><*><*><*><*><*><*><*><*><*><*>" + "\n" +
                        "memory operations are as follows: " + "\n" +
                        "\"MC\"  clear memory  " + "\n" +
                        "\"M+\" store value to memory " + "\n" +
                         "\"MRC\" recall stored value " + "\n" +
                         "<*><*><*><*><*><*><*><*><*><*><*><*>";
-               return memHelp;
+              // return memHelp;
            case "--trig help":
-               String trigHelp =
+             //  String trigHelp =
+               return
                        "<*><*><*><*><*><*><*><*><*><*><*><*>" + "\n" +
                        "\"sin or cos or tan or invsin\"  " + "\n" +
                        "\"or invtan or invcos\" for trig " + "\n" +
                        "<*><*><*><*><*><*><*><*><*><*><*><*>";
-               return trigHelp;
+              // return trigHelp;
            case "--log help":
-               String logHelp =
+            //   String logHelp =
+               return
                        "<*><*><*><*><*><*><*><*><*><*><*><*>" + "\n" +
                        "\"log or nlog or -log or -nlog\" " + "\n" +
                        "for logarithmic functions" + "\n" +
                        "<*><*><*><*><*><*><*><*><*><*><*><*>";
-               return logHelp;
+               //return logHelp;
            default:
                System.out.println("does not recognise");
-               return null;
+               return "try again";
 
        }
 
    }
-
-
 }
